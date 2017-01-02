15a4e8cc2007617418758eb0c6baa28d2c9975e0
Leo <MovitexLeo>
2017-01-02 09:42:47 +0100

    Maj main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	const boxAnimation = document.querySelectorAll(".box");
 
-	let randomBox_1 = Math.floor((Math.random() * boxAnimation.length));
-	let randomBox_2 = Math.floor((Math.random() * boxAnimation.length));
-	let randomBox_3 = Math.floor((Math.random() * boxAnimation.length));
-
 	let boxReveal = Math.floor((Math.random() * 3));
+	const randomBox = [];
+
+	for (var i = 0; i < 3; i++) {
+		randomBox[i] = Math.floor((Math.random() * boxAnimation.length));
+	}
 
 	class RandomBox {
 		constructor(boxs) {
@@ -14,6 +15,12 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 
 		play(revealDelay, hideDelay) {
+
+			const randomBox = [];
+
+			for (var i = 0; i < 3; i++) {
+				randomBox[i] = Math.floor((Math.random() * boxAnimation.length));
+			}
 
 			let random_1 = Math.floor((Math.random() * boxAnimation.length));
 			let random_2 = Math.floor((Math.random() * boxAnimation.length));
@@ -25,49 +32,49 @@ window.addEventListener('DOMContentLoaded', () => {
 
 				if (boxReveal === 0) {
 
-						this.boxs[random_1].classList.add('active');
-						this.boxs[random_1].classList.remove('inactive');
+						this.boxs[randomBox[0]].classList.add('active');
+						this.boxs[randomBox[0]].classList.remove('inactive');
 
 						window.setTimeout(() => {
 
-							this.boxs[random_1].classList.add('inactive');
-							this.boxs[random_1].classList.remove('active');
+							this.boxs[randomBox[0]].classList.add('inactive');
+							this.boxs[randomBox[0]].classList.remove('active');
 
 						}, hideDelay);
 
 					} else if (boxReveal === 1) {
 
-						this.boxs[random_1].classList.add('active');
-						this.boxs[random_2].classList.add('active');
-						this.boxs[random_1].classList.remove('inactive');
-						this.boxs[random_2].classList.remove('inactive');
+						this.boxs[randomBox[0]].classList.add('active');
+						this.boxs[randomBox[1]].classList.add('active');
+						this.boxs[randomBox[0]].classList.remove('inactive');
+						this.boxs[randomBox[1]].classList.remove('inactive');
 
 						window.setTimeout(() => {
 
-							this.boxs[random_1].classList.add('inactive');
-							this.boxs[random_2].classList.add('inactive');
-							this.boxs[random_1].classList.remove('active');
-							this.boxs[random_2].classList.remove('active');
+							this.boxs[randomBox[0]].classList.add('inactive');
+							this.boxs[randomBox[1]].classList.add('inactive');
+							this.boxs[randomBox[0]].classList.remove('active');
+							this.boxs[randomBox[1]].classList.remove('active');
 
 						}, hideDelay);
 
 					} else {
 
-						this.boxs[random_1].classList.add('active');
-						this.boxs[random_2].classList.add('active');
-						this.boxs[random_3].classList.add('active');
-						this.boxs[random_1].classList.remove('inactive');
-						this.boxs[random_2].classList.remove('inactive');
-						this.boxs[random_3].classList.remove('inactive');
+						this.boxs[randomBox[0]].classList.add('active');
+						this.boxs[randomBox[1]].classList.add('active');
+						this.boxs[randomBox[2]].classList.add('active');
+						this.boxs[randomBox[0]].classList.remove('inactive');
+						this.boxs[randomBox[1]].classList.remove('inactive');
+						this.boxs[randomBox[2]].classList.remove('inactive');
 
 						window.setTimeout(() => {
 
-							this.boxs[random_1].classList.add('inactive');
-							this.boxs[random_2].classList.add('inactive');
-							this.boxs[random_3].classList.add('inactive');
-							this.boxs[random_1].classList.remove('active');
-							this.boxs[random_2].classList.remove('active');
-							this.boxs[random_3].classList.remove('active');
+							this.boxs[randomBox[0]].classList.add('inactive');
+							this.boxs[randomBox[1]].classList.add('inactive');
+							this.boxs[randomBox[2]].classList.add('inactive');
+							this.boxs[randomBox[0]].classList.remove('active');
+							this.boxs[randomBox[1]].classList.remove('active');
+							this.boxs[randomBox[2]].classList.remove('active');
 
 						}, hideDelay);
 
